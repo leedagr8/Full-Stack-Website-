1bfb5d332915f4f59e11a0b63cb7e355ce5399c1
leedagr8 <ljohn214@uncc.edu>
2019-08-12 16:02:01 -0400

    updating databse url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-metric-71378',
         user: 'leedagr8',
         password: '19656789',
         database: 'finalProjectAPI'
